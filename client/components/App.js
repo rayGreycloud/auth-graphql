72117d631bb13a5d1ec0d9e1dfeff275194d723f
rayGreycloud <rayGreycloud@gmail.com>
2017-03-01 07:05:20 -0800

    Add boilerplate App component

---
+++ client/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+
+// Functional component because only showing header and children
+const App = (props) = {
+  return (
+    <div>
+      <Header />
+      {props.children}
+    </div>
+  );
+};
+
+export default App;
