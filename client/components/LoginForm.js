ed12e259e71d4c985c8ce5c471c57015bbc2d4bd
rayGreycloud <rayGreycloud@gmail.com>
2017-03-02 08:36:20 -0800

    Import Login mutation into LoginForm

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import AuthForm from './AuthForm';
+import { graphql } from 'react-apollo';
+import mutation from '../mutations/Login';
 
 class LoginForm extends Component {
   render() {
@@ -12,4 +14,4 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm;
+export default graphql(mutation)(LoginForm);
