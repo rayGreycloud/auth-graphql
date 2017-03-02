33057ba764f119b912d6339fbcd6efc799aafa80
rayGreycloud <rayGreycloud@gmail.com>
2017-03-02 07:18:12 -0800

    Add logout mutation

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import query from '../queries/CurrentUser';
+import mutation from '../mutations/Logout';
 
 class Header extends Component {
   onLogoutClick() {
-
+    this.props.mutate({
+    refetchQueries: [{ query }]  
+    });
   }
 
   renderButtons() {
@@ -50,4 +53,6 @@ class Header extends Component {
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(
+  graphql(query)(Header)
+);
