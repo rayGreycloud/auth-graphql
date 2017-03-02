6e15d23d5a6239a203a965f67ae41f669f0a4f69
rayGreycloud <rayGreycloud@gmail.com>
2017-03-02 06:49:11 -0800

    Add renderButtons method to Header

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,13 +3,29 @@ import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
-  render() {
-    console.log(this.props.data);
+  renderButtons() {
+    const { loading, user } = this.props.data;
 
+    if (loading) { return <div />; }
+
+    if (user) {
+      return <div>Logout</div>;
+    } else {
+      return (
+        <div>You're not signed in</div>
+      );
+    }
+
+  }
+
+
+  render() {
     return (
-      <div>
-        ---THIS IS THE HEADER---
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          {this.renderButtons()}
+        </div>
+      </nav>
     );
   }
 }
