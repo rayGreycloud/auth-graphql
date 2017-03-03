b2faf98eea7b5ebbfc5b93034970be9b40b86a0a
rayGreycloud <rayGreycloud@gmail.com>
2017-03-03 08:46:25 -0800

    Add onSubmit callback to SignupForm

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -4,10 +4,19 @@ import AuthForm from './AuthForm';
 import mutation from '../mutations/Signup';
 
 class SignupForm extends Component {
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: { email, password }
+    });
+  }
+
   render() {
     return (
       <h3>Sign Up</h3>;
-      <AuthForm />
+      <AuthForm
+        errors={[]}
+        onSubmit={this.onSubmit.bind(this)}
+      />
     );
   }
 }
