0a1bb4c33536b7e2598702346f34eddc5c41560e
rayGreycloud <rayGreycloud@gmail.com>
2017-03-03 08:31:57 -0800

    Add SignupForm component

---
+++ client/components/SignupForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import AuthForm from './AuthForm';
+
+class SignupForm extends Component {
+  render() {
+    return (
+      <h3>Sign Up</h3>;
+      <AuthForm />
+    );
+  }
+}
+
+export default SignupForm;
