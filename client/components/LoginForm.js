c45b5f2a21bd38c9d66185943cf1bcf16a12d514
rayGreycloud <rayGreycloud@gmail.com>
2017-03-03 06:52:23 -0800

    Add refetch to LoginForm to update header after login

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import AuthForm from './AuthForm';
 import { graphql } from 'react-apollo';
 import mutation from '../mutations/Login';
+import query from '../queries/CurrentUser';
 
 class LoginForm extends Component {
   onSubmit({ email, password }) {
     this.props.mutate({
-      variables: { email, password }
+      variables: { email, password },
+      refetchQueries: [{ query }]
     });
   }
 
