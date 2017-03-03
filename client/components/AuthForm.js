6523be177ec1f88d0b11fd2665826f16f6c0be02
rayGreycloud <rayGreycloud@gmail.com>
2017-03-03 06:31:02 -0800

    Replace form labels with placeholders

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -20,8 +20,8 @@ class AuthForm extends Component {
       <div className="row">
         <form onSubmit={this.onSubmit.bind(this)} className="col s4">
           <div className="input-field">
-            <label>Email</label>
             <input
+              placeholder="Email"
               value={this.state.email}
               onChange={e => this.setState({
                 email: e.target.value
@@ -29,8 +29,9 @@ class AuthForm extends Component {
             />
           </div>
           <div className="input-field">
-            <label>Password</label>
             <input
+              placeholder="Password"
+              type="password"
               value={this.state.password}
               onChange={e => this.setState({
                 password: e.target.value
